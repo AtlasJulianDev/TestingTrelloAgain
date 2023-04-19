c055ab20691bd08379581496d7eb1c807e4a67f3
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-19 12:14:47 +0200

    New shit go brr

--- client.js
+++ client.js
@@ -1,32 +1,20 @@
-var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
-var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
+/* global TrelloPowerUp */
 
-window.TrelloPowerUp.initialize({
-  "card-buttons": function (t, options) {
-    return [
-      {
-        icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-        text: "Estimate Size",
-        callback: function (t) {
-          return t.popup({
-            title: "Estimation",
-            url: "estimate.html",
-          });
-        },
-      },
-    ];
-  },
-  'card-detail-badges': function(t, options) {
-    return [{
-      title: 'Estimate',
-      color: 'red',
-      text: 'Large',
+var Promise = TrelloPowerUp.Promise;
+
+var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
+
+TrelloPowerUp.initialize({
+	'card-buttons': function(t, options) {
+		return [{
+			icon: BLACK_ROCKET_ICON,
+			text: 'Estimate Size',
       callback: function(t) {
         return t.popup({
           title: "Estimation",
           url: 'estimate.html',
         });
       }
-    }]
-  }
+		}];
+	},
 });
